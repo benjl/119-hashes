096408037d1bf956368d7d7a75afc6c7daf12a2a
benjl <benluijkx@gmail.com>
2025-03-31 21:07:50 -0600

    Add batch test function, benchmark, int formatting

--- find_119_rust/src/main.rs
+++ find_119_rust/src/main.rs
@@ -1,7 +1,31 @@
 use std::io;
 use sha256::digest;
+use std::env;
+use std::cmp::min;
+use std::time::SystemTime;
+use std::thread;
 
-fn count_119s(input: &str) -> u32 {
+struct BestHash {
+    number: u64,
+    hash: String,
+    count: u64
+}
+
+fn fmt_int(n: u64) -> String {
+    if n >= 1000000000000 {
+        return format!("{}T", (n as f64 / 10000000000.0).round() / 100.0);
+    } else if n >= 1000000000 {
+        return format!("{}B", (n as f64 / 10000000.0).round() / 100.0);
+    } else if n >= 1000000 {
+        return format!("{}M", (n as f64 / 10000.0).round() / 100.0);
+    } else if n >= 10000 {
+        return format!("{}K", (n as f64 / 10.0).round() / 100.0);
+    } else {
+        return format!("{}", n);
+    }
+}
+
+fn count_119s(input: &str) -> u64 {
     let mut count = 0;
     let mut state = 0;
     for c in input.chars() {
@@ -25,11 +49,36 @@ fn count_119s(input: &str) -> u32 {
     return count;
 }
 
+fn batch_test(start: u64, end: u64, best: u64) -> Vec<BestHash> {
+    let mut bests: Vec<BestHash> = Vec::new();
+    for n in start..end {
+        let h = digest(n.to_string());
+        let c = count_119s(&h);
+        if c > min(3, best) {
+            bests.push(BestHash { number: n, hash: h, count: c });
+        }
+    }
+    return bests;
+}
+
+fn benchmark() {
+    let t1 = SystemTime::now();
+    let results = batch_test(347000000, 357000000, 3);
+    let t2 = SystemTime::now();
+    let nps = (357000000.0 - 347000000.0) / t2.duration_since(t1).expect("Clock err").as_secs_f32();
+    println!("{} nps", nps);
+}
+
 fn main() {
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Soimething went wrong.");
-    let number = input.trim();
-    let h = digest(number);
-    let hcount = count_119s(&h);
-    println!("Hash of {}: {}, Count: {}", number, h, hcount);
+
+    // let args: Vec<String> = env::args().collect();
+    // dbg!(args);
+    // let mut input = String::new();
+    // io::stdin().read_line(&mut input).expect("Something went wrong.");
+    // let number = input.trim();
+    // let h = digest(number);
+    // let hcount = count_119s(&h);
+    // println!("Hash of {}: {}, Count: {}", number, h, hcount);
+
+    benchmark();
 }
